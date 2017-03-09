2229fce4557f002f7b8f7dc73868643aad6e4eb2
Khushhal Maheshwari <khushhal000maheshwari@gmail.com>
2017-03-09 15:42:54 +0530

    Add files via upload

---
+++ ac_current.ino.ino
@@ -0,0 +1,12 @@
+float reading = 0;
+float currentValue = 0;
+void setup() {
+  Serial.begin(115200);
+
+}
+void loop() {
+  reading = analogRead(A0); //Raw data reading
+  currentValue = (reading - 510) * 5 / 1024 / 0.04 - 0.34;
+  Serial.println(currentValue);
+  delay(800);
+}
